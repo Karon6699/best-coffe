8c391f58b4a8420a63cb49300834a44f7c958f95
Karon6699 <calvin085216@gmail.com>
2026-08-20 06:29:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+//Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// when hamburger menu clicked
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Click Outside The sidebar will close the nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
